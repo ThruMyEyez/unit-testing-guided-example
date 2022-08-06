82ba888b227a86085df1f6707d4144a3f42ca790
Artur Baumeister <Artur.baumeister@gmail.com>
2022-08-06 16:58:06 +0200

    Done

--- calculator.js
+++ calculator.js
@@ -1,13 +1,27 @@
 function sum(a, b) {
-  return;
+  if (typeof a !== 'number' && typeof b !== 'number') {
+    return 0;
+  } else if (typeof b !== 'number') {
+    return a - 0;
+  }
+  return a + b;
 }
 
 function subtract(a, b) {
-  return;
+  if (typeof a !== 'number' && typeof b !== 'number') {
+    return 0;
+  } else if (typeof b !== 'number') {
+    return a - 0;
+  }
+  return a - b;
 }
-
+console.log(subtract(-4));
+console.log(sum());
 function divide(a, b) {
-  return;
+  if (b === 0) {
+    throw new Error();
+  }
+  return a / b;
 }
 
 function multiply(a, b) {
